0474c68ed2217a4c2e028a738ceb23861cdeac75
priyajohnson <30722008+priyajohnson@users.noreply.github.com>
2017-08-12 12:17:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,11 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 //move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft *10;
+    img.style.marginLeft = marginLeft +'px';
+}
 img.onclick = function () {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight,100);
 };
