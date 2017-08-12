e35728f5d4778b0c503cd027cf48cb518832d0f7
priyajohnson <30722008+priyajohnson@users.noreply.github.com>
2017-08-12 12:08:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 //move the image
-var img = document.getElementById('maddi');
+var img = document.getElementById('madi');
 img.onclick = function () {
     img.style.marginLeft = '100px';
 };
