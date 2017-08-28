38ecfe4feb5900b7d57cde846d03dacd9c1ab031
priyajohnson <30722008+priyajohnson@users.noreply.github.com>
2017-08-28 12:34:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 var config = {
     user:  'priyajohnson',
     database: 'priyajohnson',
@@ -78,6 +79,16 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash(input,salt){
+    var hashed = crypto.pbkdf2Synd(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input',function(req,res){
+    var hashedString = hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString);
+});
+
 var pool = new Pool(config);
 app.get('/test-db',function(req,res)
 {
