2aee1af1525da2a284e09b9b50768632aefef715
priyajohnson <30722008+priyajohnson@users.noreply.github.com>
2017-08-28 18:34:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,36 @@ function moveRight(){
 img.onclick = function () {
     var interval = setInterval(moveRight,50);
 };*/
+
+/*
 var button = document.getElementById('counter');
+
+button.onclick = function(){
+    
+    //create a request to the counter endpoint
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState == XMLHttpRequest.DONE){
+            
+            if(request.status == 200){
+                var counter =request.responseText;
+                var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+                
+            }
+        }
+        
+        };
+    
+    // make the request
+    request.open('GET','http://priyajohnson.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+};*/
+
+var button = document.getElementById('submit_btn');
 
 button.onclick = function(){
     
