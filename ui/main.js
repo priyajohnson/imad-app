67f9b1bbf1c40c764e42ed247e6c765b21211da4
priyajohnson <30722008+priyajohnson@users.noreply.github.com>
2017-08-28 19:10:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -69,7 +69,7 @@ button.onclick = function(){
     // make the request
     var username = document.getElementById('username').value;
      var password = document.getElementById('password').value;
-    request.open('POST','http://priyajohnson.imad.hasura-app.io/LOGIN',true);
+    request.open('POST','http://priyajohnson.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username: username,password: password}));
    
